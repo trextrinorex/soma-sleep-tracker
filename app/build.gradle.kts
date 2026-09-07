b4d992ef829c2dd90753b9dbc3decf29c892423c
T.REX <prajapatidevendra2006@gmail.com>
2026-09-07 14:38:01 +0530

    Use Compose Compiler plugin (Kotlin 2.0 compatible) and remove deprecated composeOptions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.hilt.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -47,10 +48,6 @@ android {
 
     buildFeatures {
         compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
     }
 
     packaging {
